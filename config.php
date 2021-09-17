168073bc81863d6cb873759404b3da8574154e37
Bryan-Ritter <90880571+Bryan-Ritter@users.noreply.github.com>
2021-09-17 00:33:21 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,6 @@ define("DIR_HOME", "/srv/http/php/crudbryan/");
 define("URL_HOME", "http://localhost/php/crudbryan/");
 define("DIR_TEMPLATE", DIR_HOME . "templates/default/");
 define("URL_TEMPLATE", URL_HOME . "templates/default/");
-
-
 define("DB_HOST", "localhost");
 define("DB_DATABASE", "escola");
 define("DB_USER", "root");
